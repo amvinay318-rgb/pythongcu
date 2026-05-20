8350931329a81947550131cb232c72a411193d79
amvinay318-rgb <amvinay318@gmail.com>
2026-05-20 10:24:02 +0530

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,11 @@
+
+marks = [78, 85, 92, 64, 89]
+
+# Perform list operations
+marks.append(95)         # Adds 95 to the end
+marks.insert(2, 70)       # Inserts 70 at index 2
+marks.remove(64)         # Removes the element 64
+marks.sort()             # Sorts the list in ascending order
+
+# Print the updated list
+print("Updated Marks:", marks)
